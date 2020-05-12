d854d84dc9c99284728a0f4763090c55c98102b9
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2020-05-12 15:25:58 +0200

    step 1 : create your script

---
+++ example.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+#
+# * Copyright (c) 2009-2020. Authors: Cytomine SCRLFS.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+
+__author__ = "Hoyoux Renaud <renaud.hoyoux@cytomine.coop>"
+__copyright__ = "Apache 2 license. Made by Cytomine SCRLFS, Belgium, https://www.cytomine.coop/"
+__version__ = "1.0.0"
+
+# This is a sample of a software to interact with the Cytomine platform using the Cytomine Python client (https://github.com/cytomine/Cytomine-python-client).
+
+import os
+import sys
+import logging
+import shutil
+from argparse import ArgumentParser
+
+from cytomine import Cytomine
+from cytomine.models import ImageInstanceCollection
+
+
+# -----------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    logging.debug("Command: %s", sys.argv)
+
+    parser = ArgumentParser(prog="Cytomine Python client example")
+
+    # Cytomine
+    parser.add_argument('--host', dest='host', default='demo.cytomine.be', help="The Cytomine host")
+    parser.add_argument('--public_key', dest='public_key', help="The Cytomine public key")
+    parser.add_argument('--private_key', dest='private_key', help="The Cytomine private key")
+    parser.add_argument('--project_id', dest='project_id', help="The targeted project id")
+    parser.add_argument('--working_path', dest='working_path', help="The working path where files will be saved")
+    parser.add_argument('--my_integer_parameter', dest='my_integer_parameter', help="The tested input")
+    parameters, other = parser.parse_known_args(sys.argv[1:])
+
+    with Cytomine(host=parameters.host, public_key=parameters.public_key, private_key=parameters.private_key) as cytomine:
+
+        working_path = parameters.working_path
+        if not os.path.exists(working_path):
+            logging.info("Creating working directory: %s", working_path)
+            os.makedirs(working_path)
+
+        try:
+            test_int_parameter = int(parameters.my_integer_parameter)
+
+            logging.info("Display test_int_parameter %s", test_int_parameter)
+
+            # loop for images in the project 
+            images = ImageInstanceCollection().fetch_with_filter("project", parameters.project_id)
+            nb_images = len(images)
+            logging.info("# images in project: %d", nb_images)
+
+            #value between 0 and 100 that represent the progress bar displayed in the UI.
+            progress = 0
+            progress_delta = 100 / nb_images
+
+            # Go through all images
+            for (i, image) in enumerate(images):
+                image_str = "{} ({}/{})".format(image.instanceFilename, i+1, nb_images)
+
+                logging.debug("Image id: %d width: %d height: %d resolution: %f magnification: %d filename: %s", image.id,
+                              image.width, image.height, image.resolution, image.magnification, image.filename)
+
+
+
+                logging.info("Finished processing image %s", image.instanceFilename)
+                progress += progress_delta
+
+
+            output_path = os.path.join(working_path, "output.txt")
+            f= open(output_path,"w+")
+            f.write("Input given was %d\r\n" % test_int_parameter)
+            f.close() 
+
+        finally:
+            logging.debug("End")
+
+
+
